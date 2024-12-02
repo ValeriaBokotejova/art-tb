60cd3d3ae75abcfddb78575b7bec283a53b90280
tabira <valeria.bokotejova@gmail.com>
2024-12-02 23:45:08 +0100

    set up build

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,3 @@
-async function loadSection(filePath, elementId) {
-  try {
-    const response = await fetch(filePath);
-    if (!response.ok) {
-      throw new Error(`Error loading file ${filePath}: ${response.statusText}`);
-    }
-
-    const html = await response.text();
-    const element = document.getElementById(elementId);
-
-    if (element) {
-      element.innerHTML = html;
-    }
-  } catch (error) {
-    console.error(`Error loading ${filePath}:`, error);
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  if (document.getElementById('header')) {
-    loadSection('/src/components/header.html', 'header');
-  }
-  if (document.getElementById('hero')) {
-    loadSection('/src/components/hero.html', 'hero');
-  }
-  if (document.getElementById('about-me')) {
-    loadSection('/src/components/about.html', 'about-me');
-  }
-  if (document.getElementById('art-collections')) {
-    loadSection('/src/components/collections.html', 'art-collections');
-  }
-  if (document.getElementById('footer')) {
-    loadSection('/src/components/footer.html', 'footer');
-  }
-});
-
 function toggleLanguage() {
   alert('Language toggle will be implemented soon!');
 }
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        paintings: './paintings.html',
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+  return {
+    root: '.',
+    base: isDev ? '/' : '/art-tb/',
+    build: {
+      rollupOptions: {
+        input: {
+          main: './index.html',
+          paintings: './paintings.html',
+        },
       },
+      outDir: 'dist',
+      emptyOutDir: true,
     },
-  },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html', './src/**/**.js', './src/**/**.css']),
+    ],
+  };
 });
