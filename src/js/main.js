8c0dc4e181acbfdd326c50815153748587918c44
tabira <valeria.bokotejova@gmail.com>
2024-12-03 22:25:52 +0100

    add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 function toggleLanguage() {
   alert('Language toggle will be implemented soon!');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.burger-menu');
+    const menu = document.querySelector('.menu');
+    const menuBackdrop = document.querySelector('.menu-backdrop');
+    const closeMenuButton = document.querySelector('.close-menu-button');
+    const mobileNavigation = document.querySelector('.mobile-nav-list');
+
+    const toggleMenu = () => {
+        menu.classList.toggle('active');
+        menuBackdrop.classList.toggle('active');
+        menuButton.classList.toggle('active');
+    }
+
+    menuButton.addEventListener('click', toggleMenu);
+    menuBackdrop.addEventListener('click', toggleMenu);
+    closeMenuButton.addEventListener('click', toggleMenu);
+    mobileNavigation.addEventListener('click', toggleMenu);
+})
